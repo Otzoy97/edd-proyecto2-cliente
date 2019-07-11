659f4ac837e5803903ab505961ef238a98a21e80
otzoy97 <otzoy97@hotmail.com>
2019-07-10 22:03:37 -0600

    Par de llave y valor. Usada para ser insertada en un comboBoxList

---
+++ src/com/clienteedd/Elemento.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.clienteedd;
+
+/**
+ *
+ * @author otzoy
+ */
+public class Elemento {
+
+    private final int id;
+    private final String value;
+
+    public Elemento(int id, String value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
